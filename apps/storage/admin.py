4781c7b19d96849ce331ad8364f18386c66949bd
To Kirill <tio.kirill@gmail.com>
2023-05-22 01:14:23 +0300

    storage models

--- apps/storage/admin.py
+++ apps/storage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.storage.models import Folder, File, FileSignature
+
+admin.site.register(Folder)
+admin.site.register(File)
+admin.site.register(FileSignature)
